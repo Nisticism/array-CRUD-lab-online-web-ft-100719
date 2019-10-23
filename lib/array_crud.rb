db7e14f0b0ace18bfc4971ca572fea60a1cecded
Hans Foster <fosterhans@gmail.com>
2019-10-23 19:38:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.unshift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -31,5 +31,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[array.length]
+  array[-1]
 end
